73dae64f87748153e4441eab6bc27b4060020280
BoyChai <1972567225@qq.com>
2024-07-17 10:51:19 +0800

    feat: add KvDELDemo

--- etcd/kv.go
+++ etcd/kv.go
@@ -3,14 +3,16 @@ package etcd
 import (
 	"context"
 	"log"
+
+	"go.etcd.io/etcd/clientv3"
 )
 
 func KvPUTDemo() {
 	// PUT要上传三个参数 context、key、value、h中的OPTIONS
 	// OPTIONS是etcdctl put -h中的OPTIONS
 	// 例如--prev-kv就代表clientv3.WithPrevKV()
-	putRes, err := CLI.Put(context.Background(), "/Test", "AAA")
-	// putRes, err := cli.Put(context.Background(), "/Test", "AAA", clientv3.WithPrevKV())
+	// putRes, err := CLI.Put(context.Background(), "/Test", "AAA")
+	putRes, err := CLI.Put(context.Background(), "/Test", "AAA", clientv3.WithPrevKV())
 
 	if err != nil {
 		log.Fatalln(err)
@@ -31,4 +33,17 @@ func KvGETDemo() {
 	}
 	log.Println(string(getRes.Kvs[0].Key))
 	log.Println(string(getRes.Kvs[0].Value))
+}
+
+func KvDELDemo() {
+	delRes, err := CLI.Delete(context.Background(), "/Test", clientv3.WithPrevKV())
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if delRes.Deleted > 0 {
+		for _, v := range delRes.PrevKvs {
+			log.Println(string(v.Key))
+			log.Println(string(v.Value))
+		}
+	}
 }
